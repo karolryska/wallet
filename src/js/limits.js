5f0b8d2753b6a276528c63ffcbfd6d2fbbe99569
Karol Ryska <karolryska@gmail.com>
2021-03-31 23:15:45 +0200

    chore: change reloading color function

--- src/js/limits.js
+++ src/js/limits.js
@@ -1,28 +1,26 @@
-import {receipts, reloadDateSumColor} from './index'
-
 export let limits = {
     daily: 30,
-}
+};
 
 const limitsLink = document.querySelector(".settings__item--limits");
 
 limitsLink.addEventListener("click", () => {
     document.querySelector(".limits").classList.add("limits--active");
     document.querySelector(".limits__input").value = limits.daily;
-})
+});
 
 const backArrowSettings = document.querySelector(".limits__arrow");
 
 const reloadColor = () => {
-    for (let date in receipts) {
-        const currentDate = document.getElementById(date);
-        let currentDateSum = currentDate.querySelector(".day__sum");
-        reloadDateSumColor(currentDateSum);
-    }
-}
+    const daySumWrapperArray = [...document.querySelectorAll(".day__sum")]
+    daySumWrapperArray.forEach(sumWrapper => {
+        if (sumWrapper.textContent > limits.daily) sumWrapper.classList.add("day__sum--red");
+        else sumWrapper.classList.remove("day__sum--red");
+    });
+};
 
 backArrowSettings.addEventListener("click", () => {
     document.querySelector(".limits").classList.remove("limits--active");
     limits.daily = document.querySelector(".limits__input").value;
     reloadColor();
-})
+});
